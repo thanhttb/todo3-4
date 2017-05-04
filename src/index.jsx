c511eb3efdc98c5a0d51a4bb53544d8ac0c8d1c2
thanhttb_58 <tranthanhuet@gmail.com>
2017-05-04 22:07:17 +0700

    first Commit

---
+++ src/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {List, Map} from 'immutable';
+import {compose, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import {TodoAppContainer} from './components/TodoApp';
+import 'todomvc-app-css/index.css';
+
+// instantiate a new Redux store
+const store = createStore(reducer);
+// dispatch the SET_STATE action holding the desired state
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    todos: [
+      {id: 1, text: 'React', status: 'active', editing: false},
+      {id: 2, text: 'Redux', status: 'active', editing: false},
+      {id: 3, text: 'Immutable', status: 'active', editing: false},
+    ],
+    filter: 'all'
+  }
+});
+
+ReactDOM.render(
+  // pass the store down to the components
+  // Container components are about how things work (data fetching, state updates)
+  <Provider store={store}>
+    <TodoAppContainer />
+  </Provider>,
+  document.getElementById('app')
+);
